b7ff93a59080d4b314dc4c9a25f2c563b855a1d2
David Dunwoody <david.dunwoody@gmail.com>
2009-05-13 19:31:12 +0100

    Start going OO, and make item fullscreen (cribbing heavily from http://github.com/elemel/cannonball/tree)

--- lib/adastra/main.py
+++ lib/adastra/main.py
@@ -1,16 +1,23 @@
 import pyglet
 
-window = pyglet.window.Window()
+class AdAstraWindow(pyglet.window.Window):
+    def __init__(self):
+        pyglet.window.Window.__init__(self, fullscreen=True,
+                                      caption="Ad Astra")
+        self.set_mouse_visible(False)
+        self.label = pyglet.text.Label('Hello, world',
+                                       font_name='Times New Roman',
+                                       font_size=36,
+                                       x=self.width//2, y=self.height//2,
+                                       anchor_x='center', anchor_y='center')
 
-label = pyglet.text.Label('Hello, world',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+    def on_draw(self):
+        self.clear()
+        self.label.draw()
 
-@window.event
-def on_draw():
-    window.clear()
-    label.draw()
+def main():
+    window = AdAstraWindow()
+    pyglet.app.run()
 
-pyglet.app.run()
+if __name__ == "__main__":
+    main()
