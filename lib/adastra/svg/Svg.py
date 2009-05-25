d558e03d064d8b51e66e8a76b31b7d8591bd81ab
David Dunwoody <david.dunwoody@gmail.com>
2009-05-25 19:34:41 +0100

    Use translate instead of reference point

--- lib/adastra/svg/Svg.py
+++ lib/adastra/svg/Svg.py
@@ -5,22 +5,21 @@ import copy
 class Svg(object):
     def __init__(self):
         self.size = None
-        self.reference_point = None
         self.paths = []
         self.scale = None
         self.translate = None
-        self._translated_paths = None
+        self._transformed_paths = None
 
     # subtracts reference_point from the points in the paths
-    def translated_paths(self):
-        if self._translated_paths is not None:
-            return self._translated_paths
+    def transformed_paths(self):
+        if self._transformed_paths is not None:
+            return self._transformed_paths
         
-        self._translated_paths = copy.deepcopy(self.paths)
-        for path in self._translated_paths:
-            translated_points = []
+        self._transformed_paths = copy.deepcopy(self.paths)
+        for path in self._transformed_paths:
+            transformed_points = []
             for x, y in path.points:
-                translated_points.append((x - self.reference_point[0], y - self.reference_point[1]))
-            path.points = translated_points
+                transformed_points.append((x - self.translate[0], y - self.translate[1]))
+            path.points = transformed_points
         
-        return self._translated_paths
+        return self._transformed_paths
--- test/adastra/svg/SvgTest.py
+++ test/adastra/svg/SvgTest.py
@@ -5,38 +5,38 @@ from adastra.svg.Svg import *
 
 class SvgTest(unittest.TestCase):
 
-    def testTranslatesPathUsingReferencePoint(self):
+    def testTranslatesPath(self):
         svg = Svg()
-        svg.reference_point = (3,3)
+        svg.translate = (3,3)
         svg.paths = [Path(points=[(1,2), (3,4), (5,6)])]
-        points = svg.translated_paths()[0].points
+        points = svg.transformed_paths()[0].points
         self.assertEqual(points, [(-2,-1), (0,1), (2,3)])
 
-    def testTranslatesMultiplePathsUsingReferencePoint(self):
+    def testTranslatesMultiplePaths(self):
         svg = Svg()
-        svg.reference_point = (3,3)
+        svg.translate = (3,3)
         svg.paths = [Path(points=[(1,2), (3,4), (5,6)]),
                      Path(points=[(7,8), (9,1), (2,3)])]
-        paths = svg.translated_paths()
+        paths = svg.transformed_paths()
         self.assertEqual(paths[0].points, [(-2,-1), (0,1), (2,3)])
         self.assertEqual(paths[1].points, [(4,5), (6,-2), (-1,0)])
 
     def testTranslatesPathOnlyOnce(self):
         svg = Svg()
-        svg.reference_point = (3,3)
+        svg.translate = (3,3)
         svg.paths = [Path(points=[(1,2), (3,4), (5,6)])]
 
-        points = svg.translated_paths()[0].points
+        points = svg.transformed_paths()[0].points
         self.assertEqual(points, [(-2,-1), (0,1), (2,3)])
 
-        points = svg.translated_paths()[0].points
+        points = svg.transformed_paths()[0].points
         self.assertEqual(points, [(-2,-1), (0,1), (2,3)])
 
     def testPreservesAttributesOnPath(self):
         svg = Svg()
-        svg.reference_point = (3,3)
+        svg.translate = (3,3)
         svg.paths = [Path(points=[(1,2), (3,4), (5,6)])]
         svg.paths[0].label = 'foo'
 
-        label = svg.translated_paths()[0].label
+        label = svg.transformed_paths()[0].label
         self.assertEqual(label, 'foo')
