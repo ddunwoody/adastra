ff760f7d75b868d7e0572f1b998b1939c8604b01
David Dunwoody <david.dunwoody@gmail.com>
2009-05-25 22:48:16 +0100

    Invert Y-axis on transformed_paths

--- lib/adastra/svg/Svg.py
+++ lib/adastra/svg/Svg.py
@@ -6,21 +6,20 @@ class Svg(object):
     def __init__(self):
         self.size = None
         self.paths = []
-        self.scale = None
-        self.translate = None
+        self.scale = 1
+        self.translate = (0, 0)
         self._transformed_paths = None
 
     # returns a copy of paths with scale and translate applied
+    # flips y-axis to convert from SVG to Box2D coordinate system
     def transformed_paths(self):
         paths = copy.deepcopy(self.paths)
 
         for path in paths:
             transformed_points = []
             for x, y in path.points:
-                if self.translate is not None:
-                    x, y = x+self.translate[0], y+self.translate[1]
-                if self.scale is not None:
-                    x, y = x*self.scale, y*self.scale
+                x, y = x+self.translate[0], y+self.translate[1]
+                x, y = x*self.scale, y*-self.scale
                 transformed_points.append((x, y))
             path.points = transformed_points
         
--- test/adastra/svg/SvgTest.py
+++ test/adastra/svg/SvgTest.py
@@ -5,63 +5,61 @@ from adastra.svg.Svg import *
 
 class SvgTest(unittest.TestCase):
 
-    def testDoesNothingIfNoScaleOrTranslatePresent(self):
+    def testFlipsYAxisOnlyIfScaleAndTranslateNotSet(self):
         svg = Svg()
         svg.paths = [Path(points=[(1,2), (3,4), (5,6)])]
         points = svg.transformed_paths()[0].points
-        self.assertEqual(points, [(1,2), (3,4), (5,6)])
+        self.assertEqual(points, [(1,-2), (3,-4), (5,-6)])
 
-    def testTranslatesPath(self):
+    def testTranslatesPathAndFlipsY(self):
         svg = Svg()
         svg.translate = (-3,-3)
         svg.paths = [Path(points=[(1,2), (3,4), (5,6)])]
         points = svg.transformed_paths()[0].points
-        self.assertEqual(points, [(-2,-1), (0,1), (2,3)])
+        self.assertEqual(points, [(-2,1), (0,-1), (2,-3)])
 
     def testAppliesTransformToMultiplePaths(self):
         svg = Svg()
-        svg.translate = (-3,-3)
+        svg.translate = (1,2)
         svg.paths = [Path(points=[(1,2), (3,4), (5,6)]),
                      Path(points=[(7,8), (9,1), (2,3)])]
         paths = svg.transformed_paths()
-        self.assertEqual(paths[0].points, [(-2,-1), (0,1), (2,3)])
-        self.assertEqual(paths[1].points, [(4,5), (6,-2), (-1,0)])
-
-    def testTranslatesPathOnlyOnce(self):
-        svg = Svg()
-        svg.translate = (-3,-3)
-        svg.paths = [Path(points=[(1,2), (3,4), (5,6)])]
-
-        points = svg.transformed_paths()[0].points
-        self.assertEqual(points, [(-2,-1), (0,1), (2,3)])
-
-        points = svg.transformed_paths()[0].points
-        self.assertEqual(points, [(-2,-1), (0,1), (2,3)])
-
-    def testPreservesAttributesOnPath(self):
-        svg = Svg()
-        svg.translate = (-3,-3)
-        svg.paths = [Path(points=[(1,2), (3,4), (5,6)])]
-        svg.paths[0].label = 'foo'
-
-        label = svg.transformed_paths()[0].label
-        self.assertEqual(label, 'foo')
+        self.assertEqual(paths[0].points, [(2,-4), (4,-6), (6,-8)])
+        self.assertEqual(paths[1].points, [(8,-10), (10,-3), (3,-5)])
 
     def testScalesPath(self):
         svg = Svg()
         svg.scale = 0.2
         svg.paths = [Path(points=[(1,2), (3,4), (5,6)])]
         points = svg.transformed_paths()[0].points
-        for actual, expected in zip(points, [(0.2,0.4), (0.6,0.8), (1.0,1.2)]):
+        for actual, expected in zip(points, [(0.2,-0.4), (0.6,-0.8), (1.0,-1.2)]):
             self.assertAlmostEqual(actual[0], expected[0])
             self.assertAlmostEqual(actual[1], expected[1])
-            
+
+    def testAppliesScaleToMultiplePaths(self):
+        svg = Svg()
+        svg.scale = 10
+        svg.paths = [Path(points=[(1,2), (3,4), (5,6)]),
+                     Path(points=[(7,8), (9,1), (2,3)])]
+        paths = svg.transformed_paths()
+        self.assertEqual(paths[0].points, [(10,-20), (30,-40), (50,-60)])
+        self.assertEqual(paths[1].points, [(70,-80), (90,-10), (20,-30)])
+
+    def testPreservesAttributesOnPath(self):
+        svg = Svg()
+        svg.translate = (-3,-3)
+        svg.paths = [Path(points=[])]
+        svg.paths[0].label = 'foo'
+
+        label = svg.transformed_paths()[0].label
+        self.assertEqual(label, 'foo')
+
     def testScalesAndTranslatesPath(self):
         svg = Svg()
         svg.scale = 0.1
         svg.translate = (-3, -4)
         svg.paths = [Path(points=[(1,2), (3,4), (5,6)])]
         points = svg.transformed_paths()[0].points
-        for actual, expected in zip(points, [(-0.2, -0.2), (0, 0), (0.2,0.2)]):
+        for actual, expected in zip(points, [(-0.2, 0.2), (0, 0), (0.2,-0.2)]):
             self.assertEqual(actual[0], expected[0])
             self.assertEqual(actual[1], expected[1])
