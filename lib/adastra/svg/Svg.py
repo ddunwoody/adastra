edf7ae6a27a166603db1247ca7841ff2b3dd9a62
David Dunwoody <david.dunwoody@gmail.com>
2009-05-25 18:59:06 +0100

    Improve code that moves path by reference point

--- lib/adastra/svg/Svg.py
+++ lib/adastra/svg/Svg.py
@@ -1,16 +1,23 @@
 from adastra.svg.Path import *
 
+import copy
+
 class Svg(object):
     def __init__(self):
         self.size = None
         self.reference_point = None
         self.paths = []
+        self._translated_paths = None
 
     def translated_paths(self):
-       paths = []
-       for path in self.paths:
-           points = []
-           for x, y in path.points:
-               points.append((x - self.reference_point[0], y - self.reference_point[1]))
-           paths.append(Path(points=points))
-       return paths
+        if self._translated_paths is not None:
+            return self._translated_paths
+        
+        self._translated_paths = copy.deepcopy(self.paths)
+        for path in self._translated_paths:
+            translated_points = []
+            for x, y in path.points:
+                translated_points.append((x - self.reference_point[0], y - self.reference_point[1]))
+            path.points = translated_points
+        
+        return self._translated_paths
--- test/adastra/svg/SvgTest.py
+++ test/adastra/svg/SvgTest.py
@@ -9,5 +9,34 @@ class SvgTest(unittest.TestCase):
         svg = Svg()
         svg.reference_point = (3,3)
         svg.paths = [Path(points=[(1,2), (3,4), (5,6)])]
-        translated_points = svg.translated_paths()[0].points
-        self.assertEqual(translated_points, [(-2, -1), (0,1), (2, 3)])
+        points = svg.translated_paths()[0].points
+        self.assertEqual(points, [(-2,-1), (0,1), (2,3)])
+
+    def testTranslatesMultiplePathsUsingReferencePoint(self):
+        svg = Svg()
+        svg.reference_point = (3,3)
+        svg.paths = [Path(points=[(1,2), (3,4), (5,6)]),
+                     Path(points=[(7,8), (9,1), (2,3)])]
+        paths = svg.translated_paths()
+        self.assertEqual(paths[0].points, [(-2,-1), (0,1), (2,3)])
+        self.assertEqual(paths[1].points, [(4,5), (6,-2), (-1,0)])
+
+    def testTranslatesPathOnlyOnce(self):
+        svg = Svg()
+        svg.reference_point = (3,3)
+        svg.paths = [Path(points=[(1,2), (3,4), (5,6)])]
+
+        points = svg.translated_paths()[0].points
+        self.assertEqual(points, [(-2,-1), (0,1), (2,3)])
+
+        points = svg.translated_paths()[0].points
+        self.assertEqual(points, [(-2,-1), (0,1), (2,3)])
+
+    def testPreservesAttributesOnPath(self):
+        svg = Svg()
+        svg.reference_point = (3,3)
+        svg.paths = [Path(points=[(1,2), (3,4), (5,6)])]
+        svg.paths[0].label = 'foo'
+
+        label = svg.translated_paths()[0].label
+        self.assertEqual(label, 'foo')
