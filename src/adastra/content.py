e030d2520ddd16b7b6e57cc66b734e34e2b131d3
David Dunwoody <david.dunwoody@gmail.com>
2009-05-28 22:13:35 +0100

    Parse material and set friction, density, restitution from path data if present

--- src/adastra/content.py
+++ src/adastra/content.py
@@ -1,17 +1,25 @@
 from Box2D import *
 
+from adastra.material import *
+
+_materials = {'metal': Metal()}
+
 def parse_shapes(svg):
     shapes = []
     for path in svg.paths('shapes'):
         shape_def = b2PolygonDef()
         shape_def.vertices = path.points
-        shape_def.density = 1
-        shape_def.friction = 0.7
-        shape_def.restitution = 0.3
-        shapes.append(_Shape(shape_def, path.fill))
+        shape = _Shape()
+        shape.shape_def = shape_def
+        shape.color = path.fill
+        if path.data and path.data.has_key('material'):
+            material = _materials[path.data['material']]
+            shape_def.density = material.density
+            shape_def.friction = material.friction
+            shape_def.restitution = material.restitution
+        shapes.append(shape)
     return shapes
 
 class _Shape(object):
-    def __init__(self, shape_def, color):
-        self.shape_def = shape_def
-        self.color = color
+    shape_def = None
+    color = None
--- tests/adastra/content_test.py
+++ tests/adastra/content_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from adastra.content import parse_shapes
+from adastra.material import Metal
 from adastra.svg.Group import *
 from adastra.svg.Path import *
 from adastra.svg.Svg import *
@@ -12,24 +13,22 @@ class ContentTest(unittest.TestCase):
         group = Group()
         self.svg.groups['shapes'] = Group()
         self.paths = self.svg.groups['shapes'].paths
-        path = Path([(10,10), (10,20), (20,20)])
-        path.fill = 1, 0, 1
-        self.paths.append(path)
+        path1 = Path([(10,10), (10,20), (20,20)])
+        path1.fill = 1, 0, 1
+        path1.data = {'material': 'metal'}
+        self.paths.append(path1)
+        path2 = Path([(10,10), (10,20), (20,20)])
+        self.paths.append(path2)
         
-    def testReturnsAShape(self):
-        shapes = parse_shapes(self.svg)
-        self.assertEqual(len(shapes), 1)
-
     def testReturnAShapeForEachPath(self):
-        self.paths.append(Path())
         shapes = parse_shapes(self.svg)
         self.assertEqual(len(shapes), 2)
 
-    def testIgnoresPathsNotInShapeGroup(self):
+    def testIgnoresPathsNotInShapesGroup(self):
         self.svg.groups['foo'] = Group()
         self.svg.groups['foo'].paths.append(Path())
         shapes = parse_shapes(self.svg)
-        self.assertEqual(len(shapes), 1)
+        self.assertEqual(len(shapes), 2)
 
     def testVerticesAreSet(self):
         shapes = parse_shapes(self.svg)
@@ -39,3 +38,15 @@ class ContentTest(unittest.TestCase):
     def testColorIsSet(self):
         shapes = parse_shapes(self.svg)
         self.assertEqual(shapes[0].color, (1, 0, 1))
+
+    def testColorIsNoneIfFillNotSet(self):
+        shapes = parse_shapes(self.svg)
+        self.assertEqual(shapes[1].color, None)
+
+    def testMaterial(self):
+        shapes = parse_shapes(self.svg)
+        self.assertAlmostEqual(shapes[0].shape_def.density, Metal().density)
+        self.assertAlmostEqual(shapes[0].shape_def.friction, Metal().friction)
+        self.assertAlmostEqual(shapes[0].shape_def.restitution, Metal().restitution)
+
+
