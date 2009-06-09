3c8e09a15d390f3db4b52e19158496c540897996
David Dunwoody <david.dunwoody@gmail.com>
2009-06-09 17:12:58 +0100

    implement __repr__ for Planet

--- src/adastra/Planet.py
+++ src/adastra/Planet.py
@@ -5,8 +5,8 @@ from Box2D import b2Vec2
 class Planet(object):
     def __init__(self, position=(0,0), radius=1, mass=1, color=(1,1,1)):
         self.position = b2Vec2(position)
-        self.mass = mass
         self.radius = radius
+        self.mass = mass
         self.color = color
         
     def attract(self, position, mass):
@@ -14,3 +14,6 @@ class Planet(object):
         distanceSq = vector.LengthSquared()
         vector.Normalize()
         return vector * (mass * self.mass / distanceSq)
+
+    def __repr__(self):
+        return "Planet(position=%s, radius=%s, mass=%s, color=%s)" % (self.position.tuple(), self.radius, self.mass, self.color)
