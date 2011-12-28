dce548b2fca43dff55c07dc68e078b28f9cec48b
David Dunwoody <david.dunwoody@gmail.com>
2011-12-28 19:33:46 +0000

    support undo and spike save/load

--- adastra/editor.py
+++ adastra/editor.py
@@ -6,6 +6,9 @@ import setup
 
 from pyglet.gl import glPushMatrix, glPopMatrix, GL_LINE_STRIP, glColor3f
 from pyglet import graphics
+from pyglet.window import key
+
+from yaml import dump, load
 
 class EditLayer(layer.Layer):
     is_event_handler = True
@@ -33,22 +36,40 @@ class EditLayer(layer.Layer):
         x,y = director.get_virtual_coordinates(x,y)
         self.mouse_pos = x,y 
 
+    def on_key_press(self, symbol, modifiers):
+        if symbol == key.S:
+            print "Saving polys..."
+            with open("polys.yaml", "w") as f:
+                dump(self.polys, f)
+            print "Save complete"
+        if symbol == key.L:
+            print "Loading polys..."
+            with open("polys.yaml", "r") as f:
+                self.polys = load(f)
+            print "Load complete"
+        if symbol == key.C:
+            if self.current_poly is not None:
+                self.current_poly = None
+            else:
+                if (len(self.polys) > 0):
+                    del self.polys[-1]
+
     def draw(self):
         glPushMatrix()
         self.transform()
         glColor3f(1,1,1)
         for poly in self.polys:
-            graphics.draw(len(poly)/2, GL_LINE_STRIP, ('v2f', poly))
+            graphics.draw(len(poly)/2, GL_LINE_STRIP, ("v2f", poly))
         if self.current_poly is not None:
             glColor3f(1,1,0)
-            graphics.draw(len(self.current_poly)/2, GL_LINE_STRIP, ('v2f', self.current_poly))
+            graphics.draw(len(self.current_poly)/2, GL_LINE_STRIP, ("v2f", self.current_poly))
             glColor3f(1,0,1)
-            graphics.draw(2, GL_LINE_STRIP, ('v2f', (self.current_poly[-2], self.current_poly[-1], self.mouse_pos[0], self.mouse_pos[1])))
+            graphics.draw(2, GL_LINE_STRIP, ("v2f", (self.current_poly[-2], self.current_poly[-1], self.mouse_pos[0], self.mouse_pos[1])))
         glPopMatrix()
 
 def main():
     setup.resources()
-    director.init(**setup.consts['window'])
+    director.init(**setup.consts["window"])
     director.window.set_mouse_cursor(director.window.get_system_mouse_cursor(Window.CURSOR_CROSSHAIR))
     director.run(scene.Scene(EditLayer()))
 
