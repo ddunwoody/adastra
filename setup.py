a810c8e4785d16724959640badad28e949888165
David Dunwoody <david.dunwoody@gmail.com>
2015-11-01 20:48:32 +0000

    Update setup.py to be more normal

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
-
-__version__ = '0.0.1'
+import adastra
 
 setup(
-    name = 'Ad Astra',
-    version = __version__,
-    packages = find_packages(),
-    install_requires=['cocos2d', 'numpy', 'pymunk', 'PyOpenGL', 'PyOpenGL-accelerate',],
-    tests_require=['nose'],
-    test_suite='nose.collector',
+    name='adastra',
+    version=adastra.__version__,
+    packages=find_packages(),
+    install_requires=['numpy', 'pymunk', 'cocos2d', 'pyglet'],
+    # install_requires=['numpy', 'pygame', 'pymunk'],
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'adastra=adastra.main:main',
+            'box2d=adastra.box2d_pyramid:main'
+        ]
+    }
 )
