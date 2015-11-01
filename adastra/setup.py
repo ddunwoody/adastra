a65b80eba9c67b46d13bebe4ad8b2b9d3ae55396
David Dunwoody <david.dunwoody@gmail.com>
2015-11-01 20:48:02 +0000

    No longer add fonts explicitly

--- adastra/setup.py
+++ adastra/setup.py
@@ -3,5 +3,4 @@ def resources():
     import pyglet.resource, pyglet.font
     pyglet.resource.path.append("@resources")
     pyglet.resource.reindex()
-    pyglet.resource.add_font("PressStart2P.ttf")
     pyglet.font.load("Press Start 2P")
