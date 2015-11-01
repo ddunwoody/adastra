a5e0d5f9f6c5ed83e017cb434ed143085f8dc98d
David Dunwoody <david.dunwoody@gmail.com>
2015-11-01 22:39:26 +0000

    Update requirements to include kivy

--- setup.py
+++ setup.py
@@ -5,13 +5,11 @@ setup(
     name='adastra',
     version=adastra.__version__,
     packages=find_packages(),
-    install_requires=['numpy', 'pymunk', 'cocos2d', 'pyglet'],
-    # install_requires=['numpy', 'pygame', 'pymunk'],
-    tests_require=['pytest'],
+    install_requires=['cymunk', 'cython', 'kivent_core', 'kivent_cymunk', 'kivy', 'kivy-garden', 'numpy'],
+    tests_require=['mock', 'pytest'],
     entry_points={
         'console_scripts': [
             'adastra=adastra.main:main',
-            'box2d=adastra.box2d_pyramid:main'
         ]
     }
 )
